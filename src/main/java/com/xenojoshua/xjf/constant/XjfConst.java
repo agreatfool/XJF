3f984c44375c9fe1b27861691bb1c377abd1e50b
Jonathan Dai <nightghost5078@gmail.com>
2013-05-03 10:30:51 +0800

    Save work.

---
+++ src/main/java/com/xenojoshua/xjf/constant/XjfConst.java
@@ -0,0 +1,5 @@
+package com.xenojoshua.xjf.constant;
+
+public class XjfConst {
+    public static final String XJF_LOGGER_NAME = "XJF-Logger";
+}
--- src/main/java/com/xenojoshua/xjf/log/XjfLoggerFactory.java
+++ src/main/java/com/xenojoshua/xjf/log/XjfLoggerFactory.java
@@ -1,15 +1,23 @@
 package com.xenojoshua.xjf.log;
 
+import com.xenojoshua.xjf.constant.XjfConst;
 import org.apache.log4j.Logger;
 
 public class XjfLoggerFactory {
-    private static final Logger logger = Logger.getLogger(XjfLoggerFactory.class.getName());
+    private static Logger logger = null;
+
+    public static void init(String configPath) {
+        XjfLoggerFactory.logger = Logger.getLogger(XjfConst.XJF_LOGGER_NAME);
+    }
 
     /**
      * Get instance of org.apache.log4j.Logger
      * @return Logger logger
      */
-    public static Logger get() {
+    public static Logger get() throws Exception {
+        if (XjfLoggerFactory.logger == null) {
+            throw new Exception("[Xjf System] XjfLoggerFactory not initialized");
+        }
         return XjfLoggerFactory.logger;
     }
 }
