4ffef173bec26397df4da05c7ae417407be87bf9
Jonathan Dai <nightghost5078@gmail.com>
2013-05-07 15:16:21 +0800

    Add config lib dependency.

--- src/main/java/com/xenojoshua/xjf/constant/XjfConst.java
+++ src/main/java/com/xenojoshua/xjf/constant/XjfConst.java
@@ -6,6 +6,10 @@ public class XjfConst {
     public static String XJF_LOGS_ROOT = "";
     public static String XJF_CONF_ROOT = "";
 
+    // CONFIG
+    public static final String XJF_CONFIG_TYPE_JSON = "json";
+    public static final String XJF_CONFIG_TYPE_PROPERTIES = "properties";
+
     // LOG
     public static final String XJF_LOGGER_NAME = "[Xjf Logger]";
     public static final int XJF_LOGGER_CONF_RELOAD_INTERVAL = 100000; // 10s
