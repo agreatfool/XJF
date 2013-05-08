8a50621d04cee083d43775383cbb694cbfb0bedf
Jonathan Dai <nightghost5078@gmail.com>
2013-05-08 13:53:42 +0800

    Add isPort & isNumeric

--- src/main/java/com/xenojoshua/xjf/util/XjfValidator.java
+++ src/main/java/com/xenojoshua/xjf/util/XjfValidator.java
@@ -4,8 +4,39 @@ import java.util.regex.Pattern;
 
 public class XjfValidator {
 
+    /**
+     * Validate given string is a valid ip address or not.
+     * @param ip
+     * @return valid
+     */
     public static boolean isIP(String ip) {
-        return Pattern.compile("^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])$").matcher(ip).matches();
+        return ip.matches("^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
+    }
+
+    /**
+     * Validate given int is a valid port number or not.
+     * @param port
+     * @return valid
+     */
+    public static boolean isPort(int port) {
+        boolean valid = false;
+        if (port > 1 && port < 65535) {
+            valid = true;
+        }
+        return valid;
+    }
+
+    /**
+     * Validate given string is a valid numeric string or not.<br/>
+     * This pattern of this function matches a number with optional '-' and decimal.<br/>
+     * <b>NOTE:</b><br/>
+     * Be careful with the RegEx mechanism, though, as it'll fail if your using non-latin (i.e. 0 to 9) digits.<br/>
+     * For example, arabic digits. This is because the "\d" part of the RegEx will only match [0-9] and effectively isn't internationally numerically aware.
+     * @param number
+     * @return valid
+     */
+    public static boolean isNumeric(String number) {
+        return number.matches("-?\\d+(\\.\\d+)?");
     }
 
 }
